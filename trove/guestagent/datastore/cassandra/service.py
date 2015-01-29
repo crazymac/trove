37e95facd9016edad1b7b03260ecf62ef2a4a92c
Denis Makogon <dmakogon@mirantis.com>
2015-01-29 13:57:58 +0200

    Update cassandra.yaml ownership after write_config operation
    Reasons:
     - Cassandra service fails to start due to changed permissions
       of cassandra.yaml, this was caused because new file was
       created by another user, so it is required to revert
       ownership to conf file to prevent bootstrap failure.
    
    Changes:
     - adding exec that reverts ownership
       right after config movement.
    
    Change-Id: I6baf86c0070067fae2003f074e3f536a7fad8f5f
    Closes-Bug: #1415862

--- trove/guestagent/datastore/cassandra/service.py
+++ trove/guestagent/datastore/cassandra/service.py
@@ -136,6 +136,11 @@ class CassandraApp(object):
         try:
             os.write(conf_fd, config_contents)
             execute_function("sudo", "mv", conf_path, system.CASSANDRA_CONF)
+            #TODO(denis_makogon): figure out the dynamic way to discover
+            # configs owner since it can cause errors if there
+            # no cassandra user in operating system
+            execute_function("sudo", "chown",
+                             "cassandra:cassandra", system.CASSANDRA_CONF)
         except Exception:
             LOG.exception(
                 _("Exception generating Cassandra configuration %s.") %
